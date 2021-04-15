e21f306ebe3ab3e06a75d0bc4d9486f9a3bc535d
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2021-04-15 09:51:36 +0300

    Created a new folder named components and created a ne file for rendering the grocery list.

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,16 +1,24 @@
 import React from 'react'
 
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 
 import nachos from '../data/nachos'
+
+import ListItem, {Separator} from '../components/ListItem';
+
 export default () => {
     return (
         <SafeAreaView>
-            {nachos.map((item)=>{
-
-                return <Text>{item.name}</Text>
-
-            })}
+            <ScrollView>
+                {nachos.map((item)=> (
+                    <React.Fragment key={item.id}>
+                        <ListItem 
+                            name={item.name}
+                        />
+                        <Separator />
+                    </React.Fragment>
+                ))}
+            </ScrollView>
         </SafeAreaView>
 
     )
