486d3f601d18cd6f7600e926678d857d65045dab
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2021-04-16 11:25:29 +0300

    Added a loading state.

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
-import { View, Text, SafeAreaView, ScrollView, FlatList, KeyboardAvoidingView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, FlatList, KeyboardAvoidingView, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import {v4 as uuid } from 'uuid';
 
 import nachos from '../data/nachos'
@@ -8,17 +9,47 @@ import nachos from '../data/nachos'
 import ListItem, {Separator} from '../components/ListItem';
 import AddItem from '../components/AddItem';
 
+const updateStoredCurrentList = (list) => {
+    AsyncStorage.setItem('@@GroceryList/currentList', JSON.stringify(list));
+}
+
 export default () => {
-    const [list, setList] = useState(nachos);
+    const [list, setList] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const addItem = (text) => {
-        setList([{id: uuid(), name: text}, ...list])
+        const newList = [{id: uuid(), name: text}, ...list];
+        setList(newList);
+        updateStoredCurrentList(newList);
     }
 
     const removeItem = (id) => {
         const newList = list.filter(item => item.id !== id);
         setList(newList);
+        updateStoredCurrentList(newList);
     }
+
+    useEffect(() => {
+        setTimeout(() => {
+            AsyncStorage.getItem('@@GroceryList/currentList')
+                .then(data => JSON.parse(data))
+                .then(data => {
+                    if (data) {
+                        setList(data);
+                    }
+                    setLoading(false);
+                })
+        }, 1000)
+    }, []);
+
+    if (loading) {
+        return (
+            <SafeAreaView>
+                <Text>Loading...</Text>
+            </SafeAreaView>
+        )
+    }
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <KeyboardAvoidingView
